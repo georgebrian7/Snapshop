1a1280909e6a3824c0cf69b00fb7180e6788b03b
georgebrian7 <brianmecha021@gmail.com>
2025-06-10 10:18:19 +0300

    <feat><added camera functionality>

---
+++ snapapp/apps.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.apps import AppConfig
+# from .models import ImageModel
+
+class SnapappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'snapapp'
+
---
+++ snapapp/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render,redirect
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
+# from corona.analyzer import main
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from django.core.files import File
+from django.core.files.base import ContentFile
+from django.core.files.temp import NamedTemporaryFile
+from urllib.request import urlopen
+from django.core.files.storage import default_storage
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def camera(request):
+    if request.method == 'POST':
+        image_path = request.POST["src"]
+        image = NamedTemporaryFile()
+        urlopen(image_path).read()
+        image.write(urlopen(image_path).read())
+        image.flush()
+        image = File(image)
+        name = str(image.name).split('\\')[-1]
+        name += '.jpg'  
+        image.name = name
+        with open('image.txt', 'w+') as file:
+            file.write(str(name))
+        default_storage.save('C:/Users/George Brian/repos/Snapshop/snapshop/media/a.jpg', ContentFile(urlopen(image_path).read()))
+        return HttpResponse('Done!')
+    return render(request, 'index.html')
