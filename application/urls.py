480836fd87f5160983be2cfd1c3b1ecfc5a27760
georgebrian7 <brianmecha021@gmail.com>
2025-06-16 10:40:25 +0300

    <feat><login,signup pages structure>

--- application/urls.py
+++ application/urls.py
@@ -22,10 +22,10 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include("accounts.urls")),
+    path("", include("accounts.urls")),
     path('snapapp',include('snapapp.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    # path("", TemplateView.as_view(template_name="home.html"), name="home"),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
